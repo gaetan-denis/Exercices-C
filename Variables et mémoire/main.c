bf4dc04f59361d22ea99c5cf7d604a994394ccc5
Gaëtan Denis <gaetandenis@outlook.be>
2025-03-15 23:04:11 +0100

    Added exercice 'Affichage de variable'

---
+++ Variables et mémoire/main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>;
+#include <stdlib.h>;
+
+int main () {
+ 
+// Déclaration groupée
+
+int age_utilisateur = 25, taille_utilisateur = 175;
+
+// Utilisation des spécificiteur de format pour indiquer le type de données à afficher
+
+printf("Vous avez %d ans et vous mesurez %d \n", age_utilisateur, taille_utilisateur);
+    
+    return 0;
+}
