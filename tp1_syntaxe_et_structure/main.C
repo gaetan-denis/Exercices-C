7286c3b73128297d20718dc8768d8c7f9de76012
Gaëtan Denis <gaetandenis@outlook.be>
2025-03-15 21:53:52 +0100

    Added tp1 : 'syntaxe et structure'

---
+++ tp1_syntaxe_et_structure/main.C
@@ -0,0 +1,24 @@
+// Inclusion des fichiers de la librairie STL.
+
+#include <stdio.h>; // stdio permet d'ajouter la fonction printf.
+
+/* 
+* Inclusion des fichiers de la lib stdlib
+* Permet d'ajouter des fonctions utiles pour la gestion de la mémoire
+* Permet également la manipulation de chaînes de caractères.
+*/
+
+#include <stdlib.h>;
+
+/*
+* Fonction principale d'un programme écrit en C.
+* Cette fonction retourne 0 (zéro) si tout se passe bien.
+*/
+
+int main() {
+    printf("TP 1,\"Syntaxe et structure\" \n\t Hello World!");
+
+    return 0;
+
+}
+
