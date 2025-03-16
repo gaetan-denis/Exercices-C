d0999fd85e8c2e2380193dbf45ffbebeb79a9ae0
Gaëtan Denis <gaetandenis@outlook.be>
2025-03-16 20:16:32 +0100

    Changed directories tree and add 'Principaux types de variables' exercice

---
+++ Variables et mémoire/Principaux types de variables/main.c
@@ -0,0 +1,22 @@
+#include <stdio.h>;
+#include <stdlib.h>;
+
+int main() {
+
+    int mon_int = 12;
+    printf("mon int  = %d\n", mon_int);
+    
+    //Le simple quote est utilisé en C pour délimiter des caractères individuels.
+    char mon_char ='A';
+    printf("mon char = %c\n",mon_char);
+
+    float mon_float= 3.1416;
+
+    printf("mon float =%f\n", mon_float);
+
+    double mon_double= 12.6;
+    printf("mon double = %f\n", mon_double);
+
+    return 0;
+
+}
