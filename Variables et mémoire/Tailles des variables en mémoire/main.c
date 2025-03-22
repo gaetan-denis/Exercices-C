c2b7c30763b0d50975cb886e802cd1a57df67297
Gaëtan Denis <gaetandenis@outlook.be>
2025-03-22 22:11:36 +0100

    added new exercices 'Signed et unsigned, short et long

--- Variables et mémoire/Tailles des variables en mémoire/main.c
+++ Variables et mémoire/Tailles des variables en mémoire/main.c
@@ -3,15 +3,39 @@
 
 int main (){
     
-    int mon_int = 12;
     
     /* 
     * La fonction sizeof() retourne la taille de la mémoire utilisée par la variable
     * Prend un type de variable en paramètre.  
     */
 
-    int taille_int= sizeof(int);
-    printf("mon int = %d et prend en memoire %d octets\n",mon_int, taille_int);
+    /*
+    * Le type short prend en mémoire seulement deux octets.
+    */ 
+    
+    short mon_short=10;
+    printf("mon short= %d et prend en memoire %d octets\n",mon_short,sizeof(short));
+   
+    /*
+    * Le type long prend en mémoire quatre octets.
+    */
+
+    long mon_long=123;
+    printf("mon long = %d et prend en memoire %d octets\n",mon_long,sizeof(long));
+    
+    
+    int mon_int=-12;
+    printf("mon int = %d et prend en memoire %d octets\n",mon_int,sizeof(int));
+   
+    /*
+    * Le type int unsigned ne peut contenir de signe moins (-) comparé à int qui est signed par défaut.
+    * Cela possède un influence sur les plages de valeurs possibles pour les variables.
+    * En effet, si la taille en octets (2 ou 4 octets) reste la même,
+    * la plage des valeurs positives, elle, se retrouve doublée pour les entiers non signés.
+    */       
+
+    unsigned int mon_unsigned=12;
+    printf("mon int = %d et prend en memoire %d octets\n",mon_unsigned,sizeof(int));
     
     /*
     * sizeof() retourne un size_t, qui est un type non signé. 
