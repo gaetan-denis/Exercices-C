b400459d03b79f3305bcf2eb72b57b813bd16b95
Gaëtan Denis <gaetandenis@outlook.be>
2025-03-23 21:21:14 +0100

    added new exercice 'Les opérateurs'

---
+++ exercices/operation_et_variables/main.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int points_vie = 10;
+    printf("Points de vie = %d\n", points_vie);
+   
+   //Réaffectation de variable. 
+
+    points_vie = 5;
+    printf("Points de vie = %d\n", points_vie);
+    
+    // Addition
+
+    printf("5+3 = %d\n",5+3);
+   
+    // Soustraction
+
+    printf("5-3 = %d\n",5-3);
+   
+    // Multiplication
+
+    printf("5*3 = %d\n",5*3);
+   
+    // Division
+
+    printf("5/3 = %d\n",5/3);
+    
+    // Modulo
+
+    printf("5%%3 = %d\n",5%3);
+    return 0;
+}
